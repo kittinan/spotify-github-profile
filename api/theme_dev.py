327935cff4c32832f151082326f1f4dff1a70937
null <tunxedge@gmail.com>
2020-10-13 11:21:26 +0700

    add api for dev theme

---
+++ api/theme_dev.py
@@ -0,0 +1,62 @@
+from flask import Flask, Response, jsonify, render_template, redirect, request
+from base64 import b64decode, b64encode
+
+from view import generate_css_bar, load_image_b64
+
+app = Flask(__name__)
+
+
+def make_svg(artist_name, song_name, img, is_now_playing, cover_image):
+
+    print("make_svg")
+
+    height = 445 if cover_image else 145
+    num_bar = 75
+
+    if is_now_playing:
+        title_text = "Now playing"
+        content_bar = "".join(["<div class='bar'></div>" for i in range(num_bar)])
+    else:
+        title_text = "Recently played"
+        content_bar = ""
+
+    css_bar = generate_css_bar(num_bar)
+
+    rendered_data = {
+        "height": height,
+        "num_bar": num_bar,
+        "content_bar": content_bar,
+        "css_bar": css_bar,
+        "title_text": title_text,
+        "artist_name": artist_name,
+        "song_name": song_name,
+        "img": img,
+        "cover_image": cover_image,
+    }
+
+    return render_template("spotify.html.j2", **rendered_data)
+
+
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
+def catch_all(path):
+
+    artist_name = "Spotify Github Profile"
+    song_name = "Revolution"
+
+    img_url = "https://avatars1.githubusercontent.com/u/144775?s=300&v=4"
+    img = load_image_b64(img_url)
+    is_now_playing = True
+    cover_image = True
+
+    svg = make_svg(artist_name, song_name, img, is_now_playing, cover_image)
+
+    resp = Response(svg, mimetype="image/svg+xml")
+    resp.headers["Cache-Control"] = "s-maxage=1"
+
+    return resp
+
+
+if __name__ == "__main__":
+    # python api/theme_dev.py
+    app.run(debug=True, port=5004)
