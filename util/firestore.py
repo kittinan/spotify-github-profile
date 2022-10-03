2ee52e103f6f1f7fee313144ca6cdff44be469a2
SG5 <sergeyv89@gmail.com>
2022-10-03 07:31:21 -0700

    allow to run template dev server without firebase token

---
+++ util/firestore.py
@@ -0,0 +1,17 @@
+import json
+import os
+from base64 import b64decode
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+
+def get_firestore_db():
+    firebase_config = os.getenv("FIREBASE")
+    firebase_dict = json.loads(b64decode(firebase_config))
+
+    cred = credentials.Certificate(firebase_dict)
+    firebase_admin.initialize_app(cred)
+
+    return firestore.client()
